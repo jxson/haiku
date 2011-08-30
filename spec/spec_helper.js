9dda5ae02aec33a48d4dd381cfba26e40c7ceee3
Jason Campbell <jason@greatergood.cc>
2011-08-29 19:20:52 -0700

    alright fonally getting to the adding content bit

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -3,21 +3,18 @@
 
   require.paths.unshift(path.join(__dirname, '..', 'lib'));
 
-  // var haiku = require('haiku')
+  beforeEach(function(){
 
-
-
-  // beforeEach(function(){
-  //   this.addMatchers({
-  //     toHaveProperty: function(prop) {
-  //       try {
-  //         return prop in this.actual;
-  //       }
-  //       catch (e) {
-  //         return false;
-  //       }
-  //     }
-  //   });
-  // });
+    // this.addMatchers({
+    //   toHaveProperty: function(prop) {
+    //     try {
+    //       return prop in this.actual;
+    //     }
+    //     catch (e) {
+    //       return false;
+    //     }
+    //   }
+    // });
+  });
 
 })();
