62eab4c86f07fc62fc48539ed95c79a8a2e2bab3
Jason Campbell <jason@greatergood.cc>
2011-09-06 15:34:49 -0700

    tests for dir methods

--- test/haiku_test.js
+++ test/haiku_test.js
@@ -5,9 +5,13 @@ var helper = require('./test_helper')
   , testCase = require('nodeunit').testCase
 ;
 
-exports['new Haiku({ //... })'] = testCase({
+exports['Haiku'] = testCase({
   setUp: function(callback){
-    this.haiku = new Haiku()
+    this.haiku = new Haiku({
+      source: path.resolve(path.join('examples', 'basic'))
+    });
+
+    this.sourcepath = path.join(process.cwd(), 'examples/basic');
 
     callback();
   },
@@ -26,7 +30,6 @@ exports['new Haiku({ //... })'] = testCase({
 
     test.done();
   },
-
   defaults: function(test){
     var haiku = this.haiku;
 
@@ -35,8 +38,47 @@ exports['new Haiku({ //... })'] = testCase({
     test.equal(haiku.get('publicdir'), 'public');
 
     test.done();
+  },
+  '#sourcedir()': function(test){
+    test.equal(this.haiku.sourcedir(), this.sourcepath);
+    test.done();
+  },
+  '#contentdir()': function(test){
+    var contentdir = path.join(this.sourcepath, 'content');
+
+    test.equal(this.haiku.contentdir(), contentdir);
+    test.done();
+  },
+  '#templatesdir()': function(test){
+    var templatesdir = path.join(this.sourcepath, 'templates');
+
+    test.equal(this.haiku.templatesdir(), templatesdir);
+    test.done();
+  },
+  '#publicdir()': function(test){
+    var publicdir = path.join(this.sourcepath, 'public');
+
+    test.equal(this.haiku.publicdir(), publicdir);
+    test.done();
+  },
+  '#builddir()': function(test){
+    var builddir = path.join(this.sourcepath, 'build');
+
+    this.haiku.set({ destination: 'some/random/dir' });
+
+    test.equal(this.haiku.builddir(), this.haiku.get('destination'));
+    test.done();
   }
 });
+
+// validations: function(test){
+//   var haiku = this.haiku;
+//
+//   test.equal(haiku.isValid(), false);
+//   test.equal(haiku.errors['source'], 'needs to be set');
+//
+//   test.done();
+// },
 
 exports['Haiku #hasCollection(content)'] = testCase({
   setUp: function(callback){
@@ -50,6 +92,7 @@ exports['Haiku #hasCollection(content)'] = testCase({
   'when the collection exists': function(test){
     var haiku = this.haiku;
 
+    // collection stub
     haiku.collections = { posts: [] };
 
     test.ok(haiku.hasCollection('posts'));
@@ -61,65 +104,70 @@ exports['Haiku #hasCollection(content)'] = testCase({
   }
 });
 
-exports['Haiku #addContent(content)'] = function(test){
-  test.done();
-};
+// exports['Haiku #addContent(content)'] = function(test){
+//   test.done();
+// };
 
 exports['Haiku #read()'] = testCase({
-    setUp: function(callback){
-      this.haiku = new Haiku({
-        source: path.resolve(path.join('examples', 'basic'))
-      });
+  setUp: function(callback){
+    this.haiku = new Haiku({
+      source: path.resolve(path.join('examples', 'basic'))
+    });
 
-      callback();
-    }
-  , tearDown: function(callback){ callback(); }
+    callback();
+  },
+  tearDown: function(callback){
+    callback();
+  },
+  'should be defined': function(test){
+    test.notEqual(this.haiku.read, undefined);
+    test.done();
+  },
+  'should emit a ready event': function(test){
+    var haiku = this.haiku;
 
-  , 'should be defined': function(test){
-      test.notEqual(this.haiku.read, undefined);
+    test.expect(1);
+
+    haiku.on('ready', function(){
+      test.ok(true, 'the event was emitted');
       test.done();
-    }
-  , 'should emit a ready event': function(test){
-      var haiku = this.haiku;
+    });
 
-      test.expect(1);
+    haiku.read();
+  },
+  'should populate the haiku.partials object': function(test){
+    var haiku = this.haiku;
 
-      haiku.on('ready', function(){
-        test.ok(true, 'the event was emitted');
-        test.done();
-      });
+    test.expect(3);
 
-      haiku.read();
-    }
-  , 'should populate the haiku.partials object': function(test){
-      var haiku = this.haiku;
+    haiku.on('ready', function(){
+      test.ok(true, 'the event was emitted');
+      test.notEqual(haiku.partials, undefined);
+      test.equal(_.size(haiku.partials), 1);
+      test.done();
+    });
 
-      test.expect(3);
+    haiku.read();
+  },
+  'should populate the haiku.layouts object': function(test){
+    var haiku = this.haiku;
 
-      haiku.on('ready', function(){
-        test.ok(true, 'the event was emitted');
-        test.notEqual(haiku.partials, undefined);
-        test.equal(_.size(haiku.partials), 1);
-        test.done();
-      });
+    test.expect(3);
 
-      haiku.read();
-    }
-  , 'should populate the haiku.layouts object': function(test){
-      var haiku = this.haiku;
+    haiku.on('ready', function(){
+      test.ok(true, 'the event was emitted');
+      test.notEqual(haiku.partials, undefined);
+      test.equal(_.size(haiku.partials), 1);
+      test.done();
+    });
 
-      test.expect(3);
-
-      haiku.on('ready', function(){
-        test.ok(true, 'the event was emitted');
-        test.notEqual(haiku.partials, undefined);
-        test.equal(_.size(haiku.partials), 1);
-        test.done();
-      });
-
-      haiku.read();
-    }
+    haiku.read();
+  }
 });
+
+// exports['Haiku #build()'] = testCase({
+//
+// });
 
 if (module == require.main) {
   require('nodeunit').reporters.default.run(['test']);
