793a1369586d12c85ef146f183cbbbbc058ef372
Dan Yoder <dan@zeraweb.com>
2011-09-08 16:04:44 -0700

    Improved the naming convention for content hashes

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var H = require("./lib/haiku");
 var h = new H({ logger: { level: "debug" }, directories: { content: "../rocket.ly/content", templates: "../rocket.ly/templates" }});
-h.on("ready",function() { console.log("READY"); console.log(h); });
+h.on("ready",function() { console.timeEnd("haiku"); console.log(h); });
 h.on("error",function(err) { console.log(err.message); console.log(err.stack); })
+console.time("haiku");
 h.read();
