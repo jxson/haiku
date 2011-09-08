396b18b9f912e50ddf964e252b0ea246e3d58020
Jason Campbell <jason@greatergood.cc>
2011-09-07 18:25:11 -0700

    unit tests

--- test/haiku/content_test.js
+++ test/haiku/content_test.js
@@ -1,14 +1,28 @@
 var helper = require('../test_helper')
   , Haiku = require('haiku')
+  , Content = require('haiku/content')
   , path = require('path')
   , _ = require('underscore')
   , testCase = require('nodeunit').testCase
 ;
 
-exports.woot = function(test){
-  test.ok(true)
-  test.done()
-}
+exports['Content #read()'] = function(test){
+  var source = path.resolve(path.join('examples', 'basic'))
+    , indexpath = path.resolve(path.join(source, 'content', 'index.mustache'))
+    , haiku = new Haiku({ source: source })
+    , content = new Content({ file: indexpath })
+  ;
+
+  test.expect(2);
+  test.ok(content.read);
+
+  content.on('ready', function(){
+    test.ok(true);
+    test.done();
+  });
+
+  content.read();
+};
 
 if (module == require.main) {
   var filename = __filename.replace(process.cwd(), '');
