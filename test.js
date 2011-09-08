293f5e9b5f15887dbaa80f92c177ae4796a6816b
Dan Yoder <dan@zeraweb.com>
2011-09-08 16:16:29 -0700

    Moved path-to-name function into a separate utlities file

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var H = require("./lib/haiku");
 var h = new H({ logger: { level: "debug" }, directories: { content: "../rocket.ly/content", templates: "../rocket.ly/templates" }});
-h.on("ready",function() { console.timeEnd("haiku"); console.log(h); });
+h.on("ready",function() { 
+  console.timeEnd("haiku"); 
+  console.log(h.resolve("blog/posts/native-apps-are-dead")); 
+});
 h.on("error",function(err) { console.log(err.message); console.log(err.stack); })
 console.time("haiku");
 h.read();
