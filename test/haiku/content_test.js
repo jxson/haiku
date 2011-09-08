20a62b826193bfb6b19ea2984684e24709768570
Jason Campbell <jason@greatergood.cc>
2011-09-07 18:54:50 -0700

    tests for Content#parser()

--- test/haiku/content_test.js
+++ test/haiku/content_test.js
@@ -6,23 +6,73 @@ var helper = require('../test_helper')
   , testCase = require('nodeunit').testCase
 ;
 
-exports['Content #read()'] = function(test){
-  var source = path.resolve(path.join('examples', 'basic'))
-    , indexpath = path.join(source, 'content', 'index.mustache')
-    , haiku = new Haiku({ source: source })
-    , content = new Content({ file: indexpath })
-  ;
+exports['Content'] = testCase({
+  setUp: function(callback){
+    var source = path.resolve(path.join('examples', 'basic'))
+      , indexpath = path.join(source, 'content', 'index.mustache')
+      , haiku = new Haiku({ source: source })
+    ;
 
-  test.expect(2);
-  test.ok(content.read);
+    this.content = new Content({ file: indexpath }, haiku);
 
-  content.on('ready', function(){
-    test.ok(true);
-    test.done();
-  });
+    callback();
+  },
+  tearDown: function(callback){
+    callback();
+  },
+  '#read()': function(test){
+    var index = this.content;
 
-  content.read();
-};
+    test.expect(2);
+    test.ok(index.read, "index.read doesn't exist");
+
+    index.on('ready', function(){
+      test.ok(true, 'ready event not fired');
+      test.done();
+    });
+
+    index.read();
+  },
+  '#parser()': {
+    'when the file attribute is set': {
+      'with markdown extensions': function(test){
+        var index = this.content;
+
+        _.each(['.md', '.markdown', '.mdown', '.mkdn', '.mkd'], function(ext){
+          index.set({ file: 'markdown-file' + ext });
+
+          test.equal(index.parser(), 'markdown');
+        });
+
+        test.done();
+      },
+      'with textile extensions': function(test){
+        var index = this.content;
+
+        index.set({ file: 'textile-file.textile' });
+
+        test.equal(index.parser(), 'textile');
+        test.done();
+      },
+      'with html extensions': function(test){
+        var index = this.content;
+
+        _.each(['.mustache', '.html'], function(ext){
+          index.set({ file: 'html-file' + ext });
+
+          test.equal(index.parser(), undefined);
+        });
+
+        test.done();
+      }
+    },
+    'when the file attribute is NOT set': function(test){
+      test.ok(this.content.parser);
+      test.equal(this.content.parser(), undefined);
+      test.done();
+    }
+  }
+});
 
 exports['Content #extractAttributesFromFile(callback)'] = testCase({
   setUp: function(callback){
