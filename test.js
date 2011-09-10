ce68cc714892d899b31e832f25791fbfab7dadf8
Dan Yoder <dan@zeraweb.com>
2011-09-09 17:44:00 -0700

    Added support for partials

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var path = require('path');
 require.paths.unshift(path.join(__dirname, '.'));
 
-require("./lib/haiku/server").run({
+require("./lib/haiku").Server.run({
   address: "127.0.0.1", port: 1337,
+  logger: { 
+    level: "debug" 
+  }, 
   haiku: {
-    logger: { 
-      level: "debug" 
-    }, 
     directories: { 
       content: "../rocket.ly/content", 
       templates: "../rocket.ly/templates",
