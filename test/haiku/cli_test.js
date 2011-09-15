4484cc56224283c87262688ab186db6b3f6659ab
Jason Campbell <jason@greatergood.cc>
2011-09-15 16:10:30 -0700

    test that the right help message is displayed when doing: haiku server --help

--- test/haiku/cli_test.js
+++ test/haiku/cli_test.js
@@ -1,6 +1,7 @@
 var helper = require('../test_helper')
   , vows = require('vows')
   , assert = require('assert')
+  , sinon = require('sinon')
   , cli = require('haiku/cli')
 ;
 
@@ -8,8 +9,18 @@ vows.describe('haiku/cli').addBatch({
   'commands': {
     topic: cli,
     'server': {
+      topic: cli,
       'with no options': 'pending',
-      'with the `help` option': 'pending',
+      'with the `help` option': function(cli){
+        var sys = require('sys');
+
+        sinon.stub(sys, 'puts');
+
+        cli.server({ help: true });
+
+        assert.ok(sys.puts.called);
+        assert.equal(sys.puts.args[0], cli.help.server);
+      },
       'with the `config` option': 'pending'
     },
     'build': {
