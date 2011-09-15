03d1f5eed0b8e8402b509c2160bd76fc2146183f
Jason Campbell <jason@greatergood.cc>
2011-09-15 12:42:04 -0700

    added test stubs for test/haiku/server_test.js and test/haiku/site_test.js

--- test/all.js
+++
@@ -1,3 +0,0 @@
-var reporter = require('nodeunit').reporters.default;
-
-reporter.run(['test', 'test/haiku']);
---
+++ test/haiku/site_test.js
@@ -0,0 +1,11 @@
+var helper = require('./test_helper')
+  , vows = require('vows')
+  , assert = require('assert')
+  , haiku = require('haiku')
+;
+
+vows.describe('haiku/cli').addBatch({
+  '': {
+
+  }
+}).export(module);
