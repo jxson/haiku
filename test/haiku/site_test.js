d1f9a1f5b7baa1285bd5782a5d639dcf0b603c22
Jason Campbell <jason@greatergood.cc>
2011-09-16 17:08:35 -0700

    tests and code for haiku.Site default options and directories

--- test/haiku/site_test.js
+++ test/haiku/site_test.js
@@ -1,17 +1,75 @@
 var helper = require('../test_helper')
   , vows = require('vows')
   , assert = require('assert')
-  , haiku = require('haiku')
+  , Site = require('haiku/site')
+  , Logger = require('haiku/logger')
+  , path = require('path')
 ;
 
 vows.describe('haiku.Site').addBatch({
-  'new haiku.Site(options)': {
-    'with *default* options': 'pending',
-    'with the `root` option': 'pending',
-    'with the `destination` option': 'pending',
-    'with the `directories.content` option': 'pending',
-    'with the `directories.templates` option': 'pending',
-    'with the `directories.public` option': 'pending'
+  'A new instance\'s': {
+    '*default* `options`': {
+      topic: function(site){
+        var site = new(Site);
+        return site.options;
+      },
+      '`root` should be `process.cwd()`': function(options){
+        assert.ok(options.root);
+        assert.equal(options.root, process.cwd());
+      },
+      '`buildDir` should be "build"': function(options){
+        assert.ok(options.buildDir);
+        assert.equal(options.buildDir, 'build');
+      },
+      '`contentDir` should be "content"': function(options){
+        assert.ok(options.contentDir);
+        assert.equal(options.contentDir, 'content');
+      },
+      '`templatesDir` should be "templates"': function(options){
+        assert.ok(options.templatesDir);
+        assert.equal(options.templatesDir, 'templates');
+      },
+      '`publicDir` should be "public"': function(options){
+        assert.ok(options.publicDir);
+        assert.equal(options.publicDir, 'public');
+      },
+      '`loglevel` should be "info"': function(options){
+        assert.ok(options.loglevel);
+        assert.equal(options.loglevel, 'info');
+      },
+      '`logger` should be a logger object': function(options){
+        assert.isObject(options.logger);
+        assert.instanceOf(options.logger, Logger);
+      },
+      '`index` should be "index"': function(options){
+        assert.ok(options.index);
+        assert.equal(options.index, 'index.html');
+      }
+    },
+    '*default* `directories`': {
+      topic: function(site){
+        var site = new(Site);
+        return site.directories;
+      },
+      'has property `content`': function(directories){
+        var _path = path.join(process.cwd(), 'content');
+
+        assert.include(directories, 'content');
+        assert.equal(directories.content, _path);
+      },
+      'has property `templates`': function(directories){
+        var _path = path.join(process.cwd(), 'templates');
+
+        assert.include(directories, 'templates');
+        assert.equal(directories.templates, _path);
+      },
+      'has property `public`': function(directories){
+        var _path = path.join(process.cwd(), 'public');
+
+        assert.include(directories, 'public');
+        assert.equal(directories.public, _path);
+      }
+    }
   },
   '#read()': 'pending',
   '#find() / .resolve()': 'pending',
