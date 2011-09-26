7422e1c8850c82139393d9851d8978c832f6c331
Jason Campbell <jason@greatergood.cc>
2011-09-26 16:55:03 -0700

    test fixes to get the logger changes to jive

--- test/haiku/site_test.js
+++ test/haiku/site_test.js
@@ -15,9 +15,11 @@ var helper = require('../test_helper')
 
 vows.describe('haiku.Site').addBatch({
   'A new instance\'s': {
+    topic: function(){
+      return new(Site);
+    },
     '*default* `options`': {
       topic: function(site){
-        var site = new(Site);
         return site.options;
       },
       '`root` should be `process.cwd()`': function(options){
@@ -40,11 +42,6 @@ vows.describe('haiku.Site').addBatch({
         assert.ok(options.publicDir);
         assert.equal(options.publicDir, 'public');
       },
-      '`logger` should be a logger object': function(options){
-        assert.isObject(options.logger);
-        assert.instanceOf(options.logger, Logger);
-        assert.equal(options.logger.level, 'info');
-      },
       '`index` should be "index"': function(options){
         assert.ok(options.index);
         assert.equal(options.index, 'index.html');
@@ -56,7 +53,6 @@ vows.describe('haiku.Site').addBatch({
     },
     '*default* `directories`': {
       topic: function(site){
-        var site = new(Site);
         return site.directories;
       },
       'has property `content`': function(directories){
@@ -77,6 +73,11 @@ vows.describe('haiku.Site').addBatch({
         assert.include(directories, 'public');
         assert.equal(directories.public, _path);
       }
+    },
+    'it should have a logger': function(site){
+      assert.isObject(site.logger);
+      assert.instanceOf(site.logger, Logger);
+      assert.equal(site.logger.level, 'info');
     }
   },
   '#find(route)': {
