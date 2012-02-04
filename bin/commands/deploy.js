37620cbab6febd53738ad1d8b1633ec86864dedc
Jason Campbell <jason@spire.io>
2012-02-03 16:11:43 -0800

    working out an idea for sub-commands

--- bin/commands/deploy.js
+++ bin/commands/deploy.js
@@ -1,7 +1,19 @@
 var EE = require('events').EventEmitter
   , otimist = require('optimist')
   , command = new EE()
+  , _ = require('underscore')
+  , Target
 ;
+
+Target = function(){
+  // body...
+};
+
+Target.prototype.deploy = function(dir, callback){
+  callback(null);
+};
+
+command.actions = {};
 
 command.runner = function(){
   var argv = otimist
@@ -10,7 +22,53 @@ command.runner = function(){
       .argv
   ;
 
+  // get the list of deploy targets from the config
+
+  config = {}
+
+  config.targets = {
+    default: {
+      strategy: 's3',
+      key: 'fvdg',
+      secret: 'vzfvdatbdybhsfybh',
+      bucket: 'foo.com'
+    },
+    staging: {
+      strategy: 's3',
+      key: 'fvdg',
+      secret: 'vzfvdatbdybhsfybh',
+      bucket: 'staging.foo.com'
+    }
+  }
+
+  _.each(config.targets, function(options, name){
+    console.log(name, options);
+
+    var target = new Target(options)
+    ;
+
+    var action = new EE();
+
+    action.runner = function(){
+      target.deploy('build', function(err){
+        console.log('deployed')
+      });
+    }
+
+    command.actions[name] = action;
+  });
+
   if (argv.help) return command.emit('--help');
+
+  console.log('argv', argv._[1]);
+  console.log('command.actions', command.actions);
+
+  // show the help as the default action when there are targets
+  // walk through configuration if there are no targets
+
+  if (command.actions[argv._[1]]) {
+    command.actions[argv._[1]].runner();
+  }
 };
 
 command.on('--help', function(){
--- config.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = function(haiku){
-  haiku.configure(function(){
-
-  });
-};
