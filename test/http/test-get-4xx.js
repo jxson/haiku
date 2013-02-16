adeff4457e882d31dfd0553de7a0e1b465eb19cb
Jason Campbell <jason@js.la>
2013-02-16 00:10:34 -0800

    Tests 404s from the http handler

---
+++ test/http/test-get-4xx.js
@@ -0,0 +1,36 @@
+
+var haiku = require('../../lib')
+  , http = require('http')
+  , assert = require('assert')
+  , path = require('path')
+  , server = require('../support/server')
+  , request = require('supertest')
+
+describe('GET 404 not found', function(){
+  it('returns 404 not found', function(done){
+    request(server)
+    .get('/should-404')
+    .expect(404)
+    .end(done)
+  })
+
+  it('returns a meaningful body', function(done){
+    request(server)
+    .get('/should-404')
+    .expect('content-type', 'text/plain')
+    .expect(404, function(err, res){
+      if (err) return done(err)
+
+      assert.ok(res.text.length)
+
+      done()
+    })
+  })
+})
+
+describe('GET 406 not acceptable', function(){
+  it('returns 406 not found')
+  it('has content-length')
+})
+
+// responds 404 for non-existing content
