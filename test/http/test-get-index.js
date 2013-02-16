756967d3fbc46de7f48e1f2e19d6e0e8e674229c
Jason Campbell <jason@js.la>
2013-02-16 15:48:56 -0800

    Stubs for more http tests

---
+++ test/http/test-get-index.js
@@ -0,0 +1,18 @@
+var haiku = require('../../lib')
+  , assert = require('assert')
+  , server = require('../support/server')
+  , request = require('supertest')
+
+describe('GET /', function(){
+  it('returns 200 ok')
+
+  it('has compiled content')
+
+  describe('cache requests', function(){
+    describe('against changed entities', function(){
+      it('returns 200 ok')
+      it('has a new etag')
+      it('returns new content')
+    })
+  })
+})
