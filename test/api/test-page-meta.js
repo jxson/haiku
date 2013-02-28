3210b77125965e2224ed8873e88d5fbcc16764db
Jason Campbell <jason@js.la>
2013-02-27 23:31:26 -0800

    Adds page.meta

---
+++ test/api/test-page-meta.js
@@ -0,0 +1,35 @@
+
+var haiku = require('../../lib')
+  , assert = require('assert')
+  , path = require('path')
+
+describe('page.meta', function(){
+  before(function(){
+    var root = path.resolve(__dirname, '../fixtures/front-matter')
+
+    haiku.configure({ root: root })
+  })
+
+  it('does not require front-matter', function(done){
+    haiku.read('nothing.md', function(err, page){
+      if (err) return done(err)
+
+      assert.ok(page.meta)
+
+      done()
+    })
+  })
+
+  it('extracts from YAML', function(done){
+    haiku.read('yaml.md', function(err, page){
+      if (err) return done(err)
+
+      assert.equal(page.meta.title, 'Yo, this is some YAML')
+
+      done()
+    })
+  })
+
+  it('extracts from TOML')
+  it('extracts from JSON')
+})
