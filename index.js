75ff7cece51f88c3e565baf5391666bd5f01930e
Jason Campbell <jason@js.la>
2013-04-01 20:59:45 -0700

    Adds bunyan and powerwalk

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ var extend = require('util')._extend
   , rimraf = require('rimraf')
   , assert = require('assert')
   , path = require('path')
+  , bunyan = require('bunyan')
 
 module.exports = function(src, options){
-  var options = options || {}
-    , haiku = Object.create({ configure: configure
+  var haiku = Object.create({ configure: configure
       , read: read
       , build: build
-      }, { options: { value: {
+      , add: add
+      }, { logger: { value: false, writable: true }
+      , options: { value: {
           set src(src) { this._src = path.resolve(src) }
         , get src() { return this._src || process.cwd() }
         , 'content-dir': 'content'
         , 'build-dir': 'build'
         , 'templates-dir': 'templates'
         , 'public-dir': 'public'
-        } } })
+        , 'log-level': 'warn'
+        }, writable: true } })
 
   // https://gist.github.com/davidaurelio/838778
   extend(haiku, EE.prototype)
 
-  // options.src = src
-  haiku.configure(options)
+  if (options) haiku.configure(options)
 
   return haiku
 }
@@ -37,15 +39,56 @@ function configure(options){
     }
   })
 
+  // TODO: move this out to the CLI in case people use the API direclty
+  // and don't want the logs
+  if (! haiku.logger) {
+    haiku.logger = bunyan.createLogger({name: 'haiku'
+    , level: haiku.options['log-level']
+    })
+  }
+
+  if (haiku.logger.level() !== haiku.options['log-level']) {
+    haiku.logger.level(haiku.options['log-level'])
+  }
+
   return haiku
 }
 
 function read(){
+  var haiku = this
+    , powerwalk = require('powerwalk')
+    , content = path.resolve(haiku.options.src, haiku.options['content-dir'])
 
+  powerwalk(content)
+  .on('error', function(err){
+    haiku.emit('error', err)
+  })
+  .on('read', function(file){
+    haiku.add(file)
+  })
+  .on('end', finish)
+
+  function finish(){
+
+  }
 }
 
 function build(){
-  // remove the build dir
-  // hook up page listeners
-  // kick off the read
+  var haiku = this
+    , build = path.resolve(haiku.options.src, haiku.options['build-dir'])
+
+  haiku.logger.debug('starting build')
+  haiku.logger.info('removing --build-dir %s', build)
+
+  rimraf(build, function(err){
+    if (err) return haiku.emit(err)
+
+    haiku
+    .on('page', function(page){ page.write() })
+    .read()
+  })
+}
+
+function add(file){
+  var haiku = this
 }
