4fb108aca97d7364879699b7b2b0a10ec75ca98b
Jason Campbell <jason@artifact.sh>
2013-09-29 23:08:59 -0700

    WIP - reading

--- test/test-res-haiku.js
+++ test/test-res-haiku.js
@@ -2,6 +2,7 @@
 var assert = require('assert')
   , request = require('supertest')
   , server = require('./server')
+  , cheerio = require('cheerio')
 
 describe('res.haiku(url)', function(){
   it('404s non-existing content', function(done){
@@ -12,6 +13,30 @@ describe('res.haiku(url)', function(){
       assert.ok(res.text.length)
       done()
     })
+  })
+
+  it('renders markdown as html', function(done){
+    request(server)
+    .get('/')
+    .expect('content-type', 'text/html')
+    .expect('etag', '')
+    .expect('last-modified', '')
+    .expect(200, function(err, res){
+      if (err) return done(err)
+      assert.ok(res.text)
+      console.log('res.text', res.text)
+      done()
+    })
+
+    //     , $ = cheerio.load(res.text)
+
+    //   assert.equal($('title').text().trim()
+    //   , 'Just a blog for testing. | Test Blog'
+    //   , 'Page has a bad title')
+
+    //   assert.equal($('a:first-of-type').attr('href')
+    //   , '/posts/001.html'
+    //   , 'Page is missing a link to the first article')
   })
 
 /*
