3c56333f209c4e7cb23d2fa623e2454cbcc1fb8d
Jason Campbell <jason@artifact.sh>
2014-01-24 16:07:15 -0800

    Adds stub tests for h.render(key, ctx, callback)

---
+++ test/test-haiku-render.js
@@ -0,0 +1,11 @@
+
+describe('h.render(key, context, callback)', function(){
+  it('renders the page.content template')
+  it('renders with page context')
+  it('renders with haiku context')
+  it('passes context into the template')
+  it('does not squash haiku context')
+  it('applies the default layout to html')
+  it('does not apply the default layout to non-html')
+  it('renders with arbitrary templates/partials')
+})
