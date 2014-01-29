ea8fc988bc0b528649d68e7bb0084da4179c8916
Jason Campbell <jason@artifact.sh>
2014-01-28 20:34:05 -0800

    Adds basic template fetching and rendering

--- test/test-haiku-render.js
+++ test/test-haiku-render.js
@@ -1,6 +1,23 @@
 
+const haiku = require('../')
+    , assert = require('assert')
+    , resolve = require('./resolve')
+    , src = resolve('src')
+
 describe('h.render(key, context, callback)', function(){
-  it('renders the page.content template')
+  it('binds `this` to page instance')
+
+  it('renders the page.content template', function(done){
+    haiku(src)
+    .render('/basic-page.html', function(err, output){
+      if (err) return done(err)
+
+      assert(output.match('<p>Just a page.</p>'))
+
+      done()
+    })
+  })
+
   it('renders with page context')
   it('renders with haiku context')
   it('passes context into the template')
