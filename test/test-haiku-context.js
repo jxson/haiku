902df2f197842a78a840a9e3c708b861a14a2f98
Jason Campbell <jason@artifact.sh>
2014-02-05 13:06:07 -0800

    Adds stub tests for haiku.context (haiku-context.js)

---
+++ test/test-haiku-context.js
@@ -0,0 +1,16 @@
+
+const haiku = require('../')
+    , assert = require('assert')
+    , resolve = require('./resolve')
+    , src = resolve('src')
+
+describe('haiku-context/content template variable', function(){
+  it('populates the content collection')
+  it('contains pages in the --content-dir')
+  it('does not include indexes')
+  it('does not include drafts')
+  it('does not include non-enumerables')
+  it('has accessors for sub-directories')
+  it('sorts descending by date')
+  it('sorts by name if missing dates')
+})
