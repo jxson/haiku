b57412fe57edb4c93553d01977f712069a1da85c
Jason Campbell <jasoncampbell@google.com>
2014-08-07 22:51:44 -0700

    Adds more tests for haiku instance

--- test/test-haiku.js
+++ test/test-haiku.js
@@ -1,9 +1,13 @@
 
 const test = require('prova')
 const haiku = require('../')
+const EventEmitter = require('events').EventEmitter
 
 test('creating instances', function(assert) {
-  assert.equal(typeof haiku, 'function', 'should be a constructor')
-  assert.ok(haiku() instanceof haiku)
+  var h = haiku()
+
+  assert.equal(typeof haiku, 'function', 'haiku should be a constructor')
+  assert.ok(h instanceof haiku, 'should not require the "new" keyword')
+  assert.ok(h instanceof EventEmitter, 'should be an EventEmitter')
   assert.end()
 })
