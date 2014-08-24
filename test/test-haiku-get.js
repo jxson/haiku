c947edff96730517945085ca0f3b202683a0ec13
Jason Campbell <jasoncampbell@google.com>
2014-08-24 15:38:17 -0700

    Adds rough ability to search via haiku.get(...)

--- test/test-haiku-get.js
+++ test/test-haiku-get.js
@@ -3,12 +3,38 @@ const test = require('prova')
 const haiku = require('../')
 const source = require('./resolve')('source')
 
-test('h.get(key, callback) - by name', function(assert) {
-  assert.plan(2)
-
+test('h.get(url, callback)', function(assert) {
   haiku(source)
-  .get('basic-page.md', function(err, page) {
-    assert.ifError(err)
+  .get('/basic-page.html', function(err, page) {
+    assert.error(err)
     assert.equal(page.name, 'basic-page.md')
+    assert.end()
   })
 })
+
+test('h.get(name, callback)', function(assert) {
+  haiku(source)
+  .get('basic-page.md', function(err, page) {
+    assert.error(err)
+    assert.equal(page.name, 'basic-page.md')
+    assert.end()
+  })
+})
+
+// test('h.get(slug, callback)', function(assert) {
+//   haiku(source)
+//   .get('basic-page.html', function(err, page) {
+//     assert.ifError(err)
+//     assert.equal(page.name, 'basic-page.md')
+//     assert.end()
+//   })
+// })
+//
+// test('h.get(index, callback)', function(assert) {
+//   haiku(source)
+//   .get('/', function(err, page) {
+//     assert.ifError(err)
+//     assert.equal(page.name, 'index.md')
+//     assert.end()
+//   })
+// })
