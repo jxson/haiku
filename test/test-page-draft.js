2b81eb9e10eaa9125ec425e778a2868e4c8bef84
Jason Campbell <jasoncampbell@google.com>
2014-09-06 12:25:50 -0700

    Adds better tests/notes around page.draft

--- test/test-page-draft.js
+++ test/test-page-draft.js
@@ -4,6 +4,8 @@ const read = require('../lib/read')
 const assert = require('assert')
 const resolve = require('./resolve')
 const basedir = resolve('content')
+const source = resolve('source')
+const haiku = require('../')
 
 test('page.draft - defaults to false', function(assert) {
   var filename = resolve('content/defaults.md')
@@ -25,31 +27,25 @@ test('page.draft - can be overridden', function(assert) {
   })
 })
 
-// var assert = require('assert')
-//
-// describe('drafts', function(){
-//   var haiku = require('../')
-//     , path = require('path')
-//     , h
-//
-//   before(function(){
-//     h = haiku(path.resolve(__dirname, 'fixtures', 'drafts'))
-//   })
-//
-//   it('is findable', function(done){
-//     h.find('draft.md', function(err, page){
-//       if (err) return done(err)
-//       assert.ok(page)
-//       done()
-//     })
-//   })
-//
-//   it('is not included in the collections', function(done){
-//     h.find('draft.md', function(err, page){
-//       if (err) return done()
-//       assert.ok(page, 'Page should exist')
-//       assert.equal(h.context.content.length, 1)
-//       done()
-//     })
-//   })
-// })
+test('page.draft - can be retrieved', function(assert) {
+  haiku(source)
+  .get('/draft.html', function(err, page) {
+    assert.error(err)
+    assert.equal(page.draft, true)
+    assert.end()
+  })
+})
+
+// TODO: Make sure drafts don't show up in the template variable collections
+test.skip('page.draft - is not enumerable', function(assert) {
+  haiku(source)
+  .render('/index.html', function(err, html) {
+    assert.error(err)
+
+    var $ = cheerio.load(html)
+    var draft = $('a[href="/draft.html"]')
+
+    assert.ok(draft === false, 'draft pages should not be enumerable')
+    assert.end()
+  })
+})
