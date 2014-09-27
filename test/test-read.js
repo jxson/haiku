5f4b69af5c9222306527560544d51ab0e616d17e
Jason Campbell <jasoncampbell@google.com>
2014-09-27 00:09:11 -0700

    100% coverage for lib/read.js

---
+++ test/test-read.js
@@ -0,0 +1,25 @@
+
+const test = require('prova')
+const read = require('../lib/read')
+const resolve = require('./resolve')
+const basedir = resolve('content')
+
+test('read(filename, basedir) - stats error', function(assert) {
+  var filename = resolve('content/does-not-exist.md')
+
+  read(filename, basedir, function(err, page) {
+    assert.ok(err instanceof Error)
+    assert.equal(err.code, 'ENOENT')
+    assert.end()
+  })
+})
+
+test('read(filename, basedir) - read file error', function(assert) {
+  var filename = resolve('content/posts')
+
+  read(filename, basedir, function(err, page) {
+    assert.ok(err instanceof Error)
+    assert.equal(err.code, 'EISDIR')
+    assert.end()
+  })
+})
